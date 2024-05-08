f303b91723431ed334c85a993b8e0209915bf79f
Jadiel Junior <136523407+jadieljrdev@users.noreply.github.com>
2024-05-08 08:56:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  const initAnimationScroll = () => {
+    const sections = document.querySelectorAll('.js-section')
+  
+    const windowHalfSize = window.innerHeight * 0.6
+  
+    const animateScroll = () => {
+      sections.forEach(item => {
+        const sectionTop = item.getBoundingClientRect().top
+  
+        const isSectionVisible = (sectionTop - windowHalfSize) < 0
+  
+        if (isSectionVisible) {
+          item.classList.add('active')
+        } else {
+          item.classList.remove('active')
+        }
+  
+      })
+  
+    }
+  
+    animateScroll()
+    window.addEventListener('scroll', animateScroll)
+  
+}
+
+initAnimationScroll();
