cc6a267d04a5fccc42578f58954bae89af856459
Jadiel Junior <136523407+jadieljrdev@users.noreply.github.com>
2024-05-08 09:10:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('.nav_link');
-  
-    links.forEach(link => {
-      link.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-  
-        if (targetSection) {
-          const headerHeight = document.querySelector('.container').offsetHeight;
-          const targetPosition = targetSection.offsetTop - headerHeight;
-  
-          window.scrollTo({
-            top: targetPosition,
-            behavior: 'smooth'
-          });
-        }
-      });
-    });
-  });
-
-  const initAnimationScroll = () => {
+const initAnimationScroll = () => {
     const sections = document.querySelectorAll('.js-section')
   
     const windowHalfSize = window.innerHeight * 0.6
