472ecd24221ed5947115f3d4cba97b517bdeb530
Jadiel Junior <136523407+jadieljrdev@users.noreply.github.com>
2024-05-08 01:15:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-let trilho = document.getElementById('trilho');
-let body = document.getElementById('body');
-
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('light-mode')
-    body.classList.toggle('light-mode')
-})
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('.nav_link');
+  
+    links.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+  
+        if (targetSection) {
+          const headerHeight = document.querySelector('.container').offsetHeight;
+          const targetPosition = targetSection.offsetTop - headerHeight;
+  
+          window.scrollTo({
+            top: targetPosition,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
